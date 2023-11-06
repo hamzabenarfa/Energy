6f22b9dcf7c2b6874556f37280d448bc4c0de7d5
hamza <hamzabenarfa4@gmail.com>
2023-11-06 13:57:41 +0100

    add spinner

--- front/components/login.tsx
+++ front/components/login.tsx
@@ -3,15 +3,18 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
+import { Spinner } from "@/components/spinner";
 
 
 const Login = () => {  const router = useRouter();
   const [user, setUser] = useState("");
   const [pwd, setPwd] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = () => {
     if (user === "admin" && pwd === "admin") {
         router.push("/main");
+        setLoading(true);
     
     }
   };
@@ -37,13 +40,15 @@ const Login = () => {  const router = useRouter();
       </div>
       
     </form>
-    <Button
+
+    {loading ? ( <Button className="mt-4 w-full self-center"><Spinner /> </Button>) : ( <Button
        type="submit"
        onClick={onSubmit}
         className="mt-4 w-full self-center"
       >
         Log in
-      </Button>
+      </Button>)}
+   
   </div>
   );
 };
